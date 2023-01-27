b6ba149f345ccf87cee401f6d9dd5911b79757bb
Ali <eli.elizade.208@gmail.com>
2023-01-27 14:02:20 +0400

    added

---
+++ main.js
@@ -0,0 +1,14 @@
+let btnn = document.querySelector{".btnn"};
+
+ btnn.addEventListener("click",function(){
+    this.nextElementSibling.classList.remove('d-none')
+ btnn.addEventListener("click",function(){
+     this.nextElementSibling.classList.add('d-none')   
+ })
+ });
+divv.addEventListener("click",function(){
+this.parentElement.classList.add('d-none')
+});
+divv.addEventListener("click",function(){
+this.parentElement.classList.add('d-none')
+})
